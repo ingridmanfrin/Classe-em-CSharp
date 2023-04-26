8e3eb76b74b498c06933d0fd1c67caf2c350f583
ingridmanfrin <ingrid.caldeira@ITLNB109>
2023-04-26 02:53:27 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Classe_em_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelFrase = new System.Windows.Forms.Label();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelFrase
+            // 
+            this.labelFrase.AutoSize = true;
+            this.labelFrase.Location = new System.Drawing.Point(80, 92);
+            this.labelFrase.Name = "labelFrase";
+            this.labelFrase.Size = new System.Drawing.Size(35, 13);
+            this.labelFrase.TabIndex = 0;
+            this.labelFrase.Text = "label1";
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(83, 207);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(443, 530);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.labelFrase);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelFrase;
+        private System.Windows.Forms.Button btnOK;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classe_em_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string frase = "meu texto";
+
+            //labelFrase.Text = "O comprimento da frase: " + frase + " é " + frase.Length.ToString();
+
+            labelFrase.Text = frase.ToUpper();
+        }
+    }
+}
